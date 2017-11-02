78c7c59167c424e6726f783ca9a09f00dcd408ec
alex-dodoka <neksus.92@mail.ru>
2017-11-02 17:20:57 +0200

    Try to refactor.

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,46 @@
-function toDo() {
-    let task = $('input').val();
-    if (task !== "") {
-        let elem = $('<li></li>').text(task);
-        $(elem).append('<button class="rem btn small alert-danger">X</button>');
-        $('#some_tasks').append(elem);
-        $('input').val("");
-
-        $('.rem').on('click', function () {
-            let what = $(this).parent();
-            $(what).css('textDecoration', 'line-through');
-            $('#some_tasks').append($(what));
-
-        })
-
+// function toDo() {
+//     let task = $('input').val();
+//     if (task !== "") {
+//         let elem = $('<li></li>').text(task);
+//         $(elem).append('<button class="remove-task-tag btn small alert-danger">X</button>');
+//         $('#some_tasks').append(elem);
+//         $('input').val("");
+//
+//         $('.remove-task-tag').on('click', function () {
+//             let what = $(this).parent();
+//             $(what).css('textDecoration', 'line-through');
+//             $('#some_tasks').append($(what));
+//
+//         })
+//     }
+// }
+// toDO need to refactor and create Wrap function.
+function addNewTask(task,print) {
+    if (addToHtml!==""){
+        createTaskHtmlTag(newTaskDescription);
     }
 
+}
+
+function newTaskDescription() {
+    const newTask = $('input').val();
+    return newTask;
+}
+
+function createTaskHtmlTag(taskText) {
+    return $('<li></li>').text(taskText)
+}
+
+
+function removeTask(deleteThisTask,) {
+    $('.remove-task-tag').on('click', function () {
+        deleteThisTask = $(this).parent();
+        $(deleteThisTask).css('textDecoration', 'line-through');
+        $('#plase_for_task').append($(deleteThisTask));
+
+    })
+}
+
+function clearInput() {
+    $('input').val("");
 }
